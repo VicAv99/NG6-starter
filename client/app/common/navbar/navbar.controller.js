a3945a78071254b5fc856bb500accf018d9b2f72
Victor Avila <victor.avila@venturplex.com>
2018-08-24 06:12:31 -0700

    navbar search stuff

--- client/app/common/navbar/navbar.controller.js
+++ client/app/common/navbar/navbar.controller.js
@@ -1,8 +1,21 @@
 class NavbarController {
-  constructor() {
+  constructor($http) {
     this.navLinks = ['home', 'about'];
     this.name = 'ES6 STARTER';
+    this.http = $http;
+    this.requests = [];
+    this.url = 'https://levelup-json.herokuapp.com/Q'
+    this.form = {
+      search: ''
+    }
+  }
+
+  searchProjects(search) {
+    return this.http
+      .get(`${this.url}?q=${search}`);
   }
 }
+
+NavbarController.$inject = ['$http'];
 
 export default NavbarController;
