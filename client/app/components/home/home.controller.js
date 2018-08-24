f85c473d7cbfb9955960ea0bef7e61aa76a9afd5
Victor Avila <victor.avila@venturplex.com>
2018-08-23 22:38:15 -0700

    home req list

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -1,7 +1,17 @@
 class HomeController {
-  constructor() {
+  constructor($http) {
+    this.http = $http;
     this.name = 'home';
+    this.requests = [];
+    this.getHelpRequests();
+  }
+
+  getHelpRequests() {
+    return this.http.get('https://levelup-json.herokuapp.com/Q')
+      .then(res => this.requests = res.data);
   }
 }
+
+HomeController.$inject = ['$http']
 
 export default HomeController;
