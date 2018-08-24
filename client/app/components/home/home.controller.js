974f8f297fa11b8039bf097d3da7267e320a93d8
Victor Avila <victor.avila@venturplex.com>
2018-08-24 00:47:12 -0700

    crud

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -1,17 +1,90 @@
 class HomeController {
+  confirm;
   constructor($http) {
     this.http = $http;
-    this.name = 'home';
+    this.url = 'https://levelup-json.herokuapp.com/Q';
     this.requests = [];
     this.getHelpRequests();
+    this.form = {
+      id: null,
+      requester: '',
+      assignee: 'Jon',
+      project: 'Default',
+      name: '',
+      description: '',
+      createdAt: ''
+    }
   }
 
   getHelpRequests() {
-    return this.http.get('https://levelup-json.herokuapp.com/Q')
-      .then(res => this.requests = res.data);
+    return this.http
+      .get(this.url)
+      .then(res => (this.requests = res.data));
+  }
+
+  patchForm(form) {
+    this.form = form;
+  }
+
+  clearForm(form) {
+    this.form = {
+      id: null,
+      requester: '',
+      assignee: 'Jon',
+      project: 'Default',
+      name: '',
+      description: '',
+      createdAt: ''
+    };
+  }
+
+  save(req) {
+    if (req.id) {
+      this.updateRequest(req);
+    } else {
+      this.createRequest(req);
+    }
+  }
+
+  createRequest(req) {
+    return this.http
+      .post(this.url, req)
+      .then(res => {
+        this.getHelpRequests();
+      },
+        err => console.log('error', err)
+      ).then(() => this.clearForm(req));
+  }
+
+  updateRequest(req) {
+    const { $$hashKey, ...payload } = req;
+    if (!req.id) {
+      this.createRequest(payload);
+    } else {
+      return this.http
+        .patch(`${this.url}/${payload.id}`)
+        .then(res => {
+          this.getHelpRequests();
+        },
+          err => console.log('error', err)
+        ).then(() => this.clearForm(req));
+    }
+  }
+
+  deleteRequest(id) {
+    this.confirm = confirm('Are you sure you want to delete?');
+    if (this.confirm) {
+      return this.http
+        .delete(`${this.url}/${id}`)
+        .then(res => {
+          this.getHelpRequests();
+        },
+          err => console.log('error', err)
+        ).then(() => this.clearForm(req));
+    }
   }
 }
 
-HomeController.$inject = ['$http']
+HomeController.$inject = ['$http'];
 
 export default HomeController;
